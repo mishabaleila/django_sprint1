b17c1ab32c679818796abb064985167ac840aa98
Leila Bashimova <lolixplora@ya.ru>
2024-02-13 15:53:30 +0300

    Fix according to pep8

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -6,6 +6,10 @@ from django.views.generic import TemplateView
 app_name = 'pages'
 
 urlpatterns = [
-    path('pages/about/', TemplateView.as_view(template_name='pages/about.html'), name = 'about'),
-    path('pages/rules/', TemplateView.as_view(template_name='pages/rules.html'), name = 'rules'),
+    path('pages/about/',
+         TemplateView.as_view(template_name='pages/about.html'),
+         name='about'),
+    path('pages/rules/',
+         TemplateView.as_view(template_name='pages/rules.html'),
+         name='rules'),
 ]
